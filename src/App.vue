3c8822ed58eb13bf123756e76bbfd538d24cdf61
Judithrif <judithriefd@gmail.com>
2021-06-19 22:46:14 +0700

    calculatorputri

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+   <Heading/>
+   <calculator/>
+ </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Heading from "./components/Heading.vue";
+import Calculator from "./components/Calculator.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Heading,
+    Calculator
+  },
+};
 </script>
 
 <style>
@@ -21,6 +25,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message : "Calculator | by Putri Hanifah M"
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: #fff;
+}
+</style>
